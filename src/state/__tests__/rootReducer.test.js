bd71d5bea6e4e3b73c694ba7ceaac4fb2fbe314d
Jeff Go <j.go@entando.com>
2019-09-18 11:39:21 +0800

    EN6-78 content types - additional unit test for root reducer

--- src/state/__tests__/rootReducer.test.js
+++ src/state/__tests__/rootReducer.test.js
@@ -8,11 +8,43 @@ describe('root reducer store', () => {
     expect(Object.keys(state)).toHaveLength(10);
   });
 
+  it('contains the apimanager state', () => {
+    expect(state).toHaveProperty('api');
+  });
+
+  it('contains the contentModel state', () => {
+    expect(state).toHaveProperty('contentModel');
+  });
+
+  it('contains the contentType state', () => {
+    expect(state).toHaveProperty('contentType');
+  });
+
+  it('contains the currentUser state', () => {
+    expect(state).toHaveProperty('currentUser');
+  });
+
   it('contains the redux form state', () => {
     expect(state).toHaveProperty('form', {});
   });
 
+  it('contains the loading state', () => {
+    expect(state).toHaveProperty('loading');
+  });
+
+  it('contains the locale state', () => {
+    expect(state).toHaveProperty('locale');
+  });
+
   it('contains the messages state', () => {
     expect(state).toHaveProperty('messages');
+  });
+
+  it('contains the modal state', () => {
+    expect(state).toHaveProperty('modal');
+  });
+
+  it('contains the pagination state', () => {
+    expect(state).toHaveProperty('pagination');
   });
 });
