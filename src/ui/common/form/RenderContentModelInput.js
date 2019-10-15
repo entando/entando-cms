d184d77df8d65af28fdf708a840260363cb32000
Jeff Go <j.go@entando.com>
2019-10-15 14:18:29 +0800

    EN6-116 content shape field tokenizing with defined methods now working - still needs refactoring

--- src/ui/common/form/RenderContentModelInput.js
+++ src/ui/common/form/RenderContentModelInput.js
@@ -17,49 +17,111 @@ const aceOnBlur = onBlur => (_event, editor) => {
 };
 
 class RenderContentModelInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dictionaryLoaded: false,
+      dictionary: [],
+      dictList: [],
+      dictMapped: {},
+      regLastToken: '',
+    };
+  }
+
   componentDidUpdate(prevProps) {
-    const { dictionary } = this.props;
-    if (dictionary.length !== prevProps.dictionary.length) {
-      const mapped = dictionary.reduce((acc, curr) => {
-        acc[curr.code] = curr.methods;
-        return acc;
-      }, {});
-      const contentModelCompleter = {
-        getCompletions: (editor, session, pos, prefix, callback) => {
-          editor.commands.addCommand({
-            name: 'dotCommand1',
-            bindKey: { win: '.', mac: '.' },
-            exec: () => {
-              const cpos = editor.selection.getCursor();
-              const sess = editor.session;
-
-              const curLine = (sess.getDocument().getLine(cpos.row)).trim();
-              const curTokens = curLine.slice(0, cpos.column).split(/\s+/);
-              const curCmd = curTokens[0];
-              if (!curCmd) return;
-              const lastToken = curTokens[curTokens.length - 1];
-
-              editor.insert('.');
-              if (lastToken in mapped) {
-                callback(null, Object.entries(mapped[lastToken]).map(([key]) => ({
-                  caption: key,
-                  value: key,
-                  score: 10001,
-                  meta: `${lastToken} Object`,
-                })));
-              }
-            },
-          });
-          callback(null, dictionary.map(word => ({
-            caption: word.code,
-            value: word.code,
-            score: 10000,
-            meta: `${word.code} Object`,
-          })));
-        },
-      };
-      langTools.addCompleter(contentModelCompleter);
+    const { dictionary: _dict } = this.props;
+    if (_dict.length !== prevProps.dictionary.length) {
+      this.populateDictState();
+      this.initCompleter();
     }
+  }
+
+  populateDictState() {
+    const { dictionary: _dict } = this.props;
+    const dictMapped = _dict.reduce((acc, curr) => {
+      acc[curr.code] = curr.methods;
+      return acc;
+    }, {});
+
+    const dictionary = _dict.map(word => ({
+      caption: word.code,
+      value: word.code,
+      score: 10000,
+      meta: `${word.code} Object`,
+    }))
+
+    this.setState({
+      dictionaryLoaded: true,
+      dictionary,
+      dictMapped,
+      dictList: [...dictionary],
+    });
+  }
+
+  createCommands(editor) {
+    editor.commands.addCommand({
+      name: 'dotCommand1',
+      bindKey: { win: '.', mac: '.' },
+      exec: () => {
+        const { dictMapped: mapped } = this.state;
+        const cpos = editor.selection.getCursor();
+        const sess = editor.session;
+
+        const curLine = (sess.getDocument().getLine(cpos.row)).trim();
+        const curTokens = curLine.slice(0, cpos.column).split(/\s+/);
+        const curCmd = curTokens[0];
+        if (!curCmd) return;
+        const lastToken = curTokens[curTokens.length - 1];
+
+        editor.insert('.');
+        if (lastToken in mapped) {
+          this.setState({ regLastToken: lastToken });
+          console.log(Object.entries(mapped[lastToken]));
+          const dictList = Object.entries(mapped[lastToken]).map(([key]) => ({
+            caption: key,
+            value: key,
+            score: 10001,
+            meta: `${lastToken} Object`,
+          }));
+          this.setState({ dictList });
+        }
+      },
+    });
+    editor.commands.on('afterExec', (e) => {
+      if (e.command.name === 'dotCommand1') {
+        editor.execCommand('startAutocomplete');
+      }
+    });
+  }
+
+  initCompleter() {
+    let cmdwritten = false;
+    const contentModelCompleter = {
+      getCompletions: (editor, session, pos, prefix, callback) => {
+        if (!cmdwritten) {
+          cmdwritten = true;
+          this.createCommands(editor);
+        }
+        console.log('call', this.state.dictList);
+        callback(null, this.state.dictList.map((word) => {
+          const { regLastToken } = this.state;
+          if (regLastToken) {
+            word.completer = {
+              insertMatch: (editor, data) => {
+                const insertedValue = data.value;
+                console.log(insertedValue, this.state.dictMapped[regLastToken]);
+                if (insertedValue in this.state.dictMapped[regLastToken]) {
+                  this.setState({ regLastToken: '', dictList: [...this.state.dictionary] });
+                }
+                editor.completer.insertMatch({value: insertedValue});
+              },
+            };
+          }
+          return word;
+        }));
+      },
+    };
+    langTools.addCompleter(contentModelCompleter);
   }
 
   render() {
@@ -67,6 +129,7 @@ class RenderContentModelInput extends Component {
       input, meta: { touched, error }, label, help,
       labelSize, inputSize, append, prepend, alignClass,
     } = this.props;
+    const { dictionaryLoaded } = this.state;
     return (
       <div className={(touched && error) ? 'form-group has-error' : 'form-group'}>
         {
@@ -80,24 +143,26 @@ class RenderContentModelInput extends Component {
         }
         <Col xs={inputSize || 12 - labelSize}>
           {prepend}
-          <AceEditor
-            mode="html"
-            theme="tomorrow"
-            width="100%"
-            showPrintMargin={false}
-            editorProps={{
-              $blockScrolling: Infinity,
-            }}
-            style={{ border: '1px solid #ddd' }}
-            enableBasicAutocompletion
-            enableLiveAutocompletion
-            enableSnippets
-            name={input.name}
-            onBlur={aceOnBlur(input.onBlur)}
-            onChange={input.onChange}
-            onFocus={input.onFocus}
-            value={input.value}
-          />
+          {dictionaryLoaded ? (
+            <AceEditor
+              mode="html"
+              theme="tomorrow"
+              width="100%"
+              showPrintMargin={false}
+              editorProps={{
+                $blockScrolling: Infinity,
+              }}
+              style={{ border: '1px solid #ddd' }}
+              enableBasicAutocompletion
+              enableLiveAutocompletion
+              enableSnippets
+              name={input.name}
+              onBlur={aceOnBlur(input.onBlur)}
+              onChange={input.onChange}
+              onFocus={input.onFocus}
+              value={input.value}
+            />
+          ) : null}
           {append && <span className="AppendedLabel">{append}</span>}
           {touched && ((error && <span className="help-block">{error}</span>))}
         </Col>
