6afdcf3eeabbb384933174f43c696113468399de
Rax Layumas <raxkaynan@gmail.com>
2019-10-29 22:53:52 +0800

    EN6-82 Add TextAttributeField component logic

--- src/ui/edit-content/content-attributes/TextAttributeField.js
+++ src/ui/edit-content/content-attributes/TextAttributeField.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  fieldInputPropTypes,
+  fieldMetaPropTypes,
+} from 'redux-form';
 
-import attributeShape from './attributeShape';
+import TextInput from 'ui/common/form/RenderTextInput';
 
-const TextAttributeField = ({ attribute }) => {
-  const { code } = attribute;
+const TextAttributeField = ({
+  label, input, meta,
+}) => {
+  const { name, value: inputValue, onChange: inputOnChange } = input;
+  const { value: actualValue } = inputValue;
+
+  const attrInput = {
+    name,
+    value: actualValue || '',
+    onChange: (event) => {
+      inputOnChange({
+        ...inputValue,
+        value: event.target.value,
+      });
+    },
+  };
 
   return (
-    <div />
+    <TextInput
+      input={attrInput}
+      label={label}
+      meta={meta}
+    />
   );
 };
 
 TextAttributeField.propTypes = {
-  attribute: PropTypes.shape(attributeShape).isRequired,
+  label: PropTypes.node.isRequired,
+  input: PropTypes.shape(fieldInputPropTypes).isRequired,
+  meta: PropTypes.shape(fieldMetaPropTypes).isRequired,
 };
 
 export default TextAttributeField;
