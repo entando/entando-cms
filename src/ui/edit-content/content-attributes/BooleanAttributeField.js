8b50898c7453464ffcb93e2bc2fff2b43cfd54b1
Rax Layumas <raxkaynan@gmail.com>
2019-10-29 17:56:50 +0800

    EN6-82 Add BooleanAttributeField component logic

--- src/ui/edit-content/content-attributes/BooleanAttributeField.js
+++ src/ui/edit-content/content-attributes/BooleanAttributeField.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  fieldInputPropTypes,
+  fieldMetaPropTypes,
+} from 'redux-form';
 
+import RadioInput from 'ui/common/form/RenderRadioInput';
 import attributeShape from './attributeShape';
 
-const BooleanAttributeField = ({ attribute }) => {
+// TODO: validation
+const BooleanAttributeField = ({ attribute, input, meta }) => {
+  const toggleElements = [
+    { id: 'true', label: 'Yes' },
+    { id: 'false', label: 'No' },
+  ];
+
+  const { name, value: inputValue, onChange: inputOnChange } = input;
+  const { value: actualValue } = inputValue;
+
+  const attrInput = {
+    name,
+    value: actualValue || 'false',
+    onChange: (val) => {
+      inputOnChange({
+        ...inputValue,
+        value: val,
+      });
+    },
+  };
+
   const { code } = attribute;
 
   return (
-    <div />
+    <RadioInput
+      input={attrInput}
+      meta={meta}
+      toggleElement={toggleElements}
+      label={code}
+    />
   );
 };
 
 BooleanAttributeField.propTypes = {
   attribute: PropTypes.shape(attributeShape).isRequired,
+  input: PropTypes.shape(fieldInputPropTypes).isRequired,
+  meta: PropTypes.shape(fieldMetaPropTypes).isRequired,
 };
 
 export default BooleanAttributeField;
