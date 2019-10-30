c2e1a754b631cddb8b5766b342cf118d8a0fe1e8
Rax Layumas <raxkaynan@gmail.com>
2019-10-30 23:07:30 +0800

    EN6-82 Add EnumeratorAttributeField component logic

--- src/ui/edit-content/content-attributes/EnumeratorAttributeField.js
+++ src/ui/edit-content/content-attributes/EnumeratorAttributeField.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  fieldInputPropTypes,
+  fieldMetaPropTypes,
+} from 'redux-form';
 
+import SelectInput from 'ui/common/form/RenderSelectInput';
 import attributeShape from './attributeShape';
 
-const EnumeratorAttributeField = ({ attribute }) => {
-  const { code } = attribute;
+const EnumeratorAttributeField = ({
+  label, input, meta, attribute,
+}) => {
+  const optionKeys = {
+    optionValue: 'value',
+    optionDisplayName: 'displayName',
+  };
+
+  const { enumeratorStaticItems: itemsStr, enumeratorStaticItemsSeparator: separator } = attribute;
+  const options = itemsStr.split(separator).map(item => ({
+    [optionKeys.optionValue]: item,
+    [optionKeys.optionDisplayName]: item,
+  }));
+
+  const { name, value: inputValue, onChange: inputOnChange } = input;
+  const { value: actualValue } = inputValue;
+
+  const attrInput = {
+    name,
+    value: actualValue || '',
+    onChange: (event) => {
+      inputOnChange({
+        ...inputValue,
+        value: event.target.value,
+      });
+    },
+  };
 
   return (
-    <div />
+    <SelectInput
+      input={attrInput}
+      label={label}
+      meta={meta}
+      options={options}
+      {...optionKeys}
+    />
   );
 };
 
 EnumeratorAttributeField.propTypes = {
+  label: PropTypes.node.isRequired,
+  input: PropTypes.shape(fieldInputPropTypes).isRequired,
+  meta: PropTypes.shape(fieldMetaPropTypes).isRequired,
   attribute: PropTypes.shape(attributeShape).isRequired,
 };
 
