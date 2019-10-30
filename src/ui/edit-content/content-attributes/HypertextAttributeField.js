5192a5c4bbc0b8654864051d18e7ad4921bc7035
Rax Layumas <raxkaynan@gmail.com>
2019-10-30 17:21:01 +0800

    EN6-82 Add HypertextAttributeField component logic

--- src/ui/edit-content/content-attributes/HypertextAttributeField.js
+++ src/ui/edit-content/content-attributes/HypertextAttributeField.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  fieldInputPropTypes,
+  fieldMetaPropTypes,
+} from 'redux-form';
 
-import attributeShape from './attributeShape';
+import TextAreaInput from 'ui/common/form/RenderTextAreaInput';
 
-const HypertextAttributeField = ({ attribute }) => {
-  const { code } = attribute;
+const HypertextAttributeField = ({
+  label, input, meta,
+}) => {
+  const { name, value: inputValue, onChange: inputOnChange } = input;
+  const { value: actualValue } = inputValue;
+
+  const attrInput = {
+    name,
+    value: actualValue || '',
+    onChange: (event) => {
+      inputOnChange({
+        ...inputValue,
+        value: event.target.value,
+      });
+    },
+  };
 
   return (
-    <div />
+    <TextAreaInput
+      input={attrInput}
+      label={label}
+      meta={meta}
+      rows={3}
+      cols={50}
+    />
   );
 };
 
 HypertextAttributeField.propTypes = {
-  attribute: PropTypes.shape(attributeShape).isRequired,
+  label: PropTypes.node.isRequired,
+  input: PropTypes.shape(fieldInputPropTypes).isRequired,
+  meta: PropTypes.shape(fieldMetaPropTypes).isRequired,
 };
 
 export default HypertextAttributeField;
