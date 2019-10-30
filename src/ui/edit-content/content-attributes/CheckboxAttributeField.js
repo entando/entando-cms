ea150f42b8ef11efac72fb08029459307d3946af
Rax Layumas <raxkaynan@gmail.com>
2019-10-30 17:35:21 +0800

    EN6-82 Add CheckboxAttributeField component logic

--- src/ui/edit-content/content-attributes/CheckboxAttributeField.js
+++ src/ui/edit-content/content-attributes/CheckboxAttributeField.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  fieldInputPropTypes,
+  fieldMetaPropTypes,
+} from 'redux-form';
 
-import attributeShape from './attributeShape';
+import SwitchRenderer from 'ui/common/form/SwitchRenderer';
 
-const CheckboxAttributeField = ({ attribute }) => {
-  const { code } = attribute;
+const CheckboxAttributeField = ({ label, input, meta }) => {
+  const switchVals = {
+    trueValue: true,
+    falseValue: false,
+  };
+
+  const { name, value: inputValue, onChange: inputOnChange } = input;
+  const { value: actualValue } = inputValue;
+
+  const attrInput = {
+    name,
+    value: actualValue || switchVals.falseValue,
+    onChange: (val) => {
+      inputOnChange({
+        ...inputValue,
+        value: val,
+      });
+    },
+  };
 
   return (
-    <div />
+    <SwitchRenderer
+      input={attrInput}
+      meta={meta}
+      label={label}
+      {...switchVals}
+    />
   );
 };
 
 CheckboxAttributeField.propTypes = {
-  attribute: PropTypes.shape(attributeShape).isRequired,
+  label: PropTypes.node.isRequired,
+  input: PropTypes.shape(fieldInputPropTypes).isRequired,
+  meta: PropTypes.shape(fieldMetaPropTypes).isRequired,
 };
 
 export default CheckboxAttributeField;
