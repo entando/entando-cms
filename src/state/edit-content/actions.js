dd72b67bd8908b024794363c2a6474042ed97f18
Irakli Chalagashvili <i.chalagashvili@entando.com>
2020-04-24 20:19:06 +0400

    ENG-200 upgrade api

--- src/state/edit-content/actions.js
+++ src/state/edit-content/actions.js
@@ -138,7 +138,7 @@ export const sendPutEditContent = (id, editContentObject) => dispatch => new Pro
         }
       });
     })
-    .catch((err) => {}),
+    .catch(() => {}),
 );
 
 export const saveContent = values => (dispatch, getState) => new Promise((resolve) => {
