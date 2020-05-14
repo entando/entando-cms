14e414eaf3c4ce26221e2eeca15d9cf02670775b
Irakli Chalagashvili <i.chalagashvili@entando.com>
2020-05-15 00:14:02 +0400

    ENG-519 implement cms side for expand/collapse all

--- src/state/pages/types.js
+++ src/state/pages/types.js
@@ -4,3 +4,6 @@ export const ADD_PAGES = 'pages/add-pages';
 export const SET_PAGE_LOADING = 'pages/set-page-loading';
 export const SET_PAGE_LOADED = 'pages/set-page-loaded';
 export const TOGGLE_PAGE_EXPANDED = 'pages/toggle-page-expanded';
+export const CLEAR_TREE = 'pages/clear-tree';
+export const BATCH_TOGGLE_EXPANDED = 'pages/set-all-pages-expanded';
+export const COLLAPSE_ALL = 'pages/collapse-all-pages';
