9c61325748fb3727dd0105fee418fb34d0bc5bf3
Guilherme <guilherme.r89@gmail.com>
2020-10-28 09:27:11 -0300

    mocking userPreference state to not break the standalone app

--- src/state/user-preferences/selectors.js
+++ src/state/user-preferences/selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 
-export const getUserPreferences = state => state.userPreferences;
+export const getUserPreferences = state => state.userPreferences || {};
 
 export const getTranslationWarning = (
   createSelector([getUserPreferences], preferences => preferences.translationWarning)
