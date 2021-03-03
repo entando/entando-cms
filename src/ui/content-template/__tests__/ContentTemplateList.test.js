131a4967471e48f23c8bfe65306e967ca7f32fb2
Jeff Go <j.go@entando.com>
2021-03-03 12:09:31 +0800

    ENG-1796 removing manual query selectors on recent RTL unit tests

--- src/ui/content-template/__tests__/ContentTemplateList.test.js
+++ src/ui/content-template/__tests__/ContentTemplateList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { mockRenderWithRouter, mockRenderWithIntl } from 'testutils/helpers';
 import { GET_CONTENT_TEMPLATES_RESPONSE_OK as contentTemplates } from 'testutils/mocks/contentTemplate';
 
@@ -16,39 +16,40 @@ const props = {
   onClickDelete: () => {},
 };
 
-const renderComponent = () => render(mockRenderWithRouter(
+const generateWrapper = () => mockRenderWithRouter(
   mockRenderWithIntl(
     <ContentTemplateList {...props} />,
     {
       modal: { info: {}, visiableModal: '' },
     },
   ),
-));
+);
 
 describe('content-template/ContentTemplateList', () => {
   it('renders component without crashing', () => {
-    const { container } = renderComponent(<ContentTemplateList {...props} />);
-    expect(container.querySelector('.ContentTemplateList__wrap')).toBeInTheDocument();
+    render(generateWrapper(<ContentTemplateList {...props} />));
+    const actionHeader = screen.getByText('Actions');
+    expect(actionHeader).toBeTruthy();
   });
 
   it('has class ContentTemplateList__table', () => {
-    const { container } = renderComponent(<ContentTemplateList {...props} />);
-    expect(container.querySelector('.ContentTemplateList__table')).toBeInTheDocument();
+    render(generateWrapper(<ContentTemplateList {...props} />));
+    expect(screen.getByText('Actions').closest('table')).toBeTruthy();
   });
 
   it('called onDidMount and load rows same length with contentTemplates', () => {
-    const { container } = renderComponent(<ContentTemplateList {...props} />);
+    render(generateWrapper(<ContentTemplateList {...props} />));
     expect(props.onDidMount).toHaveBeenCalled();
-    expect(container.querySelectorAll('.ContentTemplateList')).toHaveLength(contentTemplates.length);
+    const table = within(screen.queryByRole('table'));
+    const resultSet = within(table.queryAllByRole('rowgroup')[1]);
+    const rows = resultSet.getAllByRole('row');
+    expect(rows).toHaveLength(contentTemplates.length);
   });
 
   it('last column is a DropdownKebab Component', () => {
-    const { container } = renderComponent(<ContentTemplateList {...props} />);
-    const last = container.querySelector('tr td:last-child');
-    expect(last.classList.contains('text-center')).toBe(true);
-    const kebab = last.querySelector('.dropdown.btn-group.dropdown-kebab-pf');
-    const tooltipBtn = kebab.querySelector('button.btn.dropdown-toggle.btn-link');
+    render(generateWrapper(<ContentTemplateList {...props} />));
+    const row = within(screen.queryAllByRole('row')[1]);
+    const kebab = row.queryByRole('button');
     expect(kebab).toBeInTheDocument();
-    expect(tooltipBtn).toBeInTheDocument();
   });
 });
