f7476a174c933521436a54f889adac3337785dde
Jeff Go <j.go@entando.com>
2021-06-01 10:55:39 +0800

    ENG-2311 revert category tree filtering exclusively for category tree selector components

--- src/ui/categories/common/CategoryTreeSelectorContainer.js
+++ src/ui/categories/common/CategoryTreeSelectorContainer.js
@@ -2,10 +2,10 @@ import { connect } from 'react-redux';
 import CategoryTreeSelector from 'ui/categories/common/CategoryTreeSelector';
 
 import { handleExpandCategory, onUnjoinCategory, onJoinCategory } from 'state/categories/actions';
-import { getCategoryTree, getJoinedCategoriesByCodes } from 'state/categories/selectors';
+import { getCategoryTreeVisibleNodes, getJoinedCategoriesByCodes } from 'state/categories/selectors';
 
 export const mapStateToProps = state => ({
-  categories: getCategoryTree(state),
+  categories: getCategoryTreeVisibleNodes(state),
   getJoinedCategoriesByCodes: getJoinedCategoriesByCodes(state),
 });
 
